28a3faa1bd2dcd93d5450eb41f795bb7d0cad81b
Valentina Galvis <Danniittasanchez@gmail.com>
2025-09-01 23:02:32 -0500

    se agrego articulo 11, se modifico el js, se modifico estilos,

--- FuncionesDan.js
+++ FuncionesDan.js
@@ -1,14 +1,16 @@
-function mostrarAlerta1() {
-    alert('Más información');
+function mostrarMas() {
+  const extra = document.getElementById("masInfo");
+  if (extra.style.display === "none") {
+    extra.style.display = "block";   // muestra el contenido
+  } else {
+    extra.style.display = "none";    // lo oculta de nuevo
+  }
 }
+
 function cambiarTema(articuloId) {
     const cont = document.getElementById(articuloId);
     if (!cont) return;
     cont.classList.toggle('modo-oscuro');
-}
-function cambiarTamaño() {
-    const texto = document.getElementById("textoCambiar");
-    texto.classList.toggle("aumentado");
 }
 function irAPaginaPrincipal() {
     window.location.href = "index.html";
@@ -18,4 +20,9 @@ function cambiarFondo(articuloId) {
     if (!articulo) return;
     articulo.style.backgroundColor =
         articulo.style.backgroundColor === "red" ? "white" : "red";
+}
+function toggleResumen() {
+  const resumen = document.getElementById("resumen");
+  if (!resumen) return;           
+  resumen.hidden = !resumen.hidden; 
 }
